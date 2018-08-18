7f65c69c0aa50c6b83a193b84dbcd5113632348b
slark101 <rusditoriq101@gmail.com>
2018-08-18 12:00:04 +0800

    Setting database into mysql engine

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'portofolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': config('DB_NAME'),
         'USER':config('DB_USER'),
         'PASSWORD':config('DB_PASSWORD'),
